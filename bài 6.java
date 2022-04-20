e06c0057d6dbdfc368871b7252dbf45761ef63b1
VUNGOCQUYNH22 <100742398+VUNGOCQUYNH22@users.noreply.github.com>
2022-04-20 15:33:18 +0700

    Create bài 6.java

---
+++ bài 6.java
@@ -0,0 +1,16 @@
+gói  helloworld ;
+nhập  java . sử dụng . Máy quét ;
+ lớp  công khai bai6 {
+    public  static  void  main ( String [] args ) {
+        Máy quét  sc = Máy quét mới  ( Hệ thống . In );
+        Hệ thống . ra ngoài . print ( "nhap vao n:" );
+        int  n = sc . nextInt ();
+       int  tong = 1 ;
+       int  i = 1 ;
+       trong khi ( i <= n ) {
+		tong * = i ;      
+		++ tôi ;
+	}
+        Hệ thống . ra ngoài . print ( "giai thua cua n:" + tong );
+}
+}
