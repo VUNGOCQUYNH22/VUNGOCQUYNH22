76ad1d7b4930cc6db15899f132cc4e87819b64c2
VUNGOCQUYNH22 <100742398+VUNGOCQUYNH22@users.noreply.github.com>
2022-04-20 15:34:11 +0700

    Create bài 7.java

---
+++ bài 7.java
@@ -0,0 +1,12 @@
+gói  helloworld ;
+nhập  java . sử dụng . Máy quét ;
+ lớp  công khai bai7 {
+    public  static  void  main ( String [] args ) {
+        for ( int  i = 1 ; i <= 6 ; i ++) {
+            Hệ thống . ra ngoài . println ( "so nguyen:" + i );
+        }
+        for ( int  i = 1 ; i <= 6 ; i ++) {
+            Hệ thống . ra ngoài . println ( i );
+        }
+}
+}
